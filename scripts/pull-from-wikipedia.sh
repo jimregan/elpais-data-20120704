c99b0b584a01b3fefdb618624f9470c4fef5d90e
Jimmy O'Regan <joregan@gmail.com>
2012-07-08 15:08:02 +0100

    crappy script

---
+++ scripts/pull-from-wikipedia.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+INF=../data/elpais_lexicalisation_frequency.txt
+TMP=../tmp
+
+_q () {
+	i=$1
+	head='<h1 id="firstHeading" class="firstHeading">'
+	raw=$(wget -O - -q http://en.wikipedia.org/wiki/$i|grep "$head"|awk -F'[<>]' '{print $5}')
+	if [ "$raw" = "Main Page" ]
+	then 
+		raw=""
+	fi
+	echo "$raw"|sed -e '
+		s/ /_/g;
+		s/(/%28/g;
+		s/)/%29/g;
+	'
+}
+
+_out () {
+	echo "<$1> <http://xmlns.com/foaf/0.1/primaryTopic> <http://dbpedia.org/resource/$2> ."
+}
+
+_fix () {
+	echo $1 | sed -e '
+		s/cion_/ción_/;
+		s/polit/polít/;
+		s/_/ /g;
+	'
+}
+
+_tr () {
+	_fix "$1"|apertium es-en|sed -e 's/ /_/g'|grep -v '\*'
+}
+
+cat $INF|awk '{print $2}'|while read raw
+do
+	i=$(_fix $raw|awk 'BEGIN{FS="/"}{print $5;}')
+	tr=$(_q $(_tr $i))
+	if [ x"$tr" != x"" ]
+	then
+		_out $raw $tr  >> $TMP/tr.txt
+	else
+		untr=$(_q $i)
+		if [ x"$untr" != x"" ]
+		then
+			_out $raw $untr  >> $TMP/untr.txt
+		else
+			echo $raw >> $TMP/todo.txt
+		fi
+	fi
+done
